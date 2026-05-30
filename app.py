0e0ff675e22d0b2c44601a9975c77be1bf394391
Bhavy Soni <bhavysoni2005@gmail.com>
2026-05-30 14:04:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,11 @@ import numpy as np
 import tensorflow as tf
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.sequence import pad_sequences
+import os
 
+
+st.write("Current Directory:", os.getcwd())
+st.write("Files Available:", os.listdir())
 # ------------------------------
 # Page Config
 # ------------------------------
